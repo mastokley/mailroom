6fa22a7f964f49151c5a3977c96de1ecdbda42eb
Kent Ross <root.main@gmail.com>
2016-03-03 17:59:39 -0800

    Finished core functionality

--- src/mailroom.py
+++ src/mailroom.py
@@ -1,6 +1,13 @@
 # coding=utf-8
 from __future__ import print_function, unicode_literals
-from builtins import input, open
+from builtins import input
+
+import re
+
+from tabulate import tabulate
+
+
+NUMBER_TEST = re.compile(r"^\$?(\d+(\.\d\d?)?)$")
 
 
 class DonorData(object):
@@ -24,55 +31,116 @@ class DonorData(object):
 
 def make_report(donors):
     """Print a list of donors, sorted by total historical donation amount"""
-    for donor in donors.all_donor_names():
+    donor_info = []
+    for donor_name in donors.all_donor_names():
+        history = donors.donor_history(donor_name)
+        total = float(sum(history))
+        donation_count = len(history)
+        average = float(total / donation_count,)
+        donor_info.append((total, donor_name, donation_count, average))
 
-    pass
+    donor_info.sort(reverse=True)
 
-
-def main():
-    donors = DonorData()
-    while main_menu(donors):
-        pass
+    return tabulate(
+        donor_info,
+        headers=[
+            "Total", "Donor", "Number of donations", "Average donation"
+        ],
+        tablefmt='grid',
+        floatfmt='.2f',
+    )
 
 
 def main_menu(donors):
-    user_input = input("""main menu message""")
-    if """user asked for report""":
-        print(make_report(donors))
-    elif """user asked to send a thank you""":
-        letter_menu(donors)
-    elif """user asked to quit""":
-        return False
-    return True
+    while True:
+        user_input = input(
+            "MAILROOM MADNESS (1: 'report', 2: 'thank', 'quit'): "
+        ).strip()
+
+        if user_input.lower() in ("report", "1"):
+            print(make_report(donors))
+        elif user_input.lower() in ("thank", "2"):
+            letter_menu(donors)
+        elif user_input.lower() in ("quit", "exit", "q", ":q"):
+            return
 
 
-def show_donor_list(donors):
-    pass
+def make_donor_list(donors):
+    return (
+        "\n"
+        "Donors:\n"
+        "-------\n"
+    ) + "\n".join(sorted(
+        donors.all_donor_names(),
+        key=lambda x: x.lower()  # sort case-insensitive
+    ))
+
+
+def parse_donation_amount(user_input):
+    """
+    Accept a string and return the amount of money it represents
+    if it was valid, or None if it wasn't
+    """
+    match = NUMBER_TEST.match(user_input)
+    if match:
+        return float(match.group(1))
+    else:
+        # input was not a number
+        return None
 
 
 def letter_menu(donors):
+    donor_name = None  # for static inspections
+    donation_amount = None
+
     while True:
-        user_input = input("""get donor name message""")
-        if """user asked to go back""":
+        user_input = input("Input donor name (or 'back', 'list'): ").strip()
+        if user_input.lower() == "back":
             return
-        elif """user asked for list""":
-            show_donor_list()
+        elif user_input.lower() == "list":
+            print(make_donor_list(donors))
+            continue
+        elif not user_input:
+            # empty string, ask again
+            continue
+        # user_input is now the donor's name
+        donor_name = user_input
+        break
+
+    while True:
+        user_input = input("Enter donation amount ('back'): ").strip()
+        if user_input.lower() == "back":
+            return  # back to main menu
+        if not user_input:
             continue
 
-    donor_name = user_input
-
-    while True:
-        user_input = input("""get donation amount message""")
-        if """user asked to go back""":
-            return
-        if """input was not a number""":
-            print("""not a number message""")
-
-    donation_amount = user_input
+        donation_amount = parse_donation_amount(user_input)
+        if not donation_amount:
+            print("'{}' is not a valid donation amount!".format(user_input))
+            continue
+        else:
+            break
 
     donors.add_donation(donor_name, donation_amount)
-    send_thank_you(donor_name, donation_amount)
+    print(make_thank_you(donor_name, donation_amount))
 
 
-def send_thank_you(donor_name, donation_amount):
-    pass
+def make_thank_you(donor_name, donation_amount):
+    return """Dear {0},
+
+    Thank you for your generous donation of ${1:.2f}. Something something,
+etc., please do it again immediately except with more money.
+
+Regards,
+
+DonationCorp Ltd.""".format(donor_name, donation_amount)
+
+
+def main():
+    """Main CLI method"""
+    donors = DonorData()
+    main_menu(donors)
+
+
+if __name__ == "__main__":
+    main()
--- src/test_mailroom.py
+++ src/test_mailroom.py
@@ -1,9 +1,32 @@
 # coding=utf-8
 from __future__ import unicode_literals
 
+from tabulate import tabulate
+
 
 def test_foo():
     assert 1 == 1
+
+
+def test_parse_donation_amount():
+    from mailroom import parse_donation_amount
+    assert parse_donation_amount("") is None
+    assert parse_donation_amount("asfd") is None
+    assert parse_donation_amount("333333w") is None
+    assert parse_donation_amount("33333333") == 33333333
+    assert parse_donation_amount("3.50") == 3.5
+    assert parse_donation_amount("$3.50") == 3.5
+    assert parse_donation_amount("$$3.50") is None
+    assert parse_donation_amount("$3.500") is None
+    assert parse_donation_amount("d444") is None
+    assert parse_donation_amount("nan") is None
+    assert parse_donation_amount("inf") is None
+    assert parse_donation_amount("-3.50") is None
+    assert parse_donation_amount("$-3.50") is None
+    assert parse_donation_amount("-$3.50") is None
+    assert parse_donation_amount(".50") is None
+    assert parse_donation_amount("0.50") == .5
+    assert parse_donation_amount("0") == 0
 
 
 def test_donor_table_functionality():
@@ -28,18 +51,76 @@ def test_donor_table_functionality():
     return dd
 
 
+def test_make_donor_list():
+    from mailroom import DonorData, make_donor_list
+    dd = DonorData()
+    assert make_donor_list(dd) == (
+        "\n"
+        "Donors:\n"
+        "-------\n"
+    )
+    dd.add_donation("John Smith", 50)
+    assert make_donor_list(dd) == (
+        "\n"
+        "Donors:\n"
+        "-------\n"
+        "John Smith"
+    )
+
+    dd.add_donation("John Smith", 500)
+    assert make_donor_list(dd) == (
+        "\n"
+        "Donors:\n"
+        "-------\n"
+        "John Smith"
+    )
+
+    dd.add_donation("Sally Smith", 500)
+    assert make_donor_list(dd) == (
+        "\n"
+        "Donors:\n"
+        "-------\n"
+        "John Smith\n"
+        "Sally Smith"
+    )
+
+    dd.add_donation("aaaaa", 3434)
+    assert make_donor_list(dd) == (
+        "\n"
+        "Donors:\n"
+        "-------\n"
+        "aaaaa\n"
+        "John Smith\n"
+        "Sally Smith"
+    )
+
+
 def test_make_report():
     import mailroom
     dd = mailroom.DonorData()
     dd.add_donation("John Smith", 50)
     dd.add_donation("John Smith", 500)
     dd.add_donation("Sally Smith", 500)
-
-    assert mailroom.make_report(dd) == (
-        "John Smith:\n"
-        "    50, 500\n"
-        "    550 total\n"
-        "Sally Smith:\n"
-        "    500\n"
-        "    500 total\n"
+    assert mailroom.make_report(dd) == tabulate(
+        (
+            (550.0, "John Smith", 2, 275.0),
+            (500.0, "Sally Smith", 1, 500.0),
+        ),
+        headers=[
+            "Total", "Donor", "Number of donations", "Average donation"
+        ],
+        tablefmt='grid',
+        floatfmt='.2f',
     )
+
+
+def test_make_thank_you():
+    from mailroom import make_thank_you
+    assert make_thank_you("asdfSDF", 343423.2399999) == """Dear asdfSDF,
+
+    Thank you for your generous donation of $343423.24. Something something,
+etc., please do it again immediately except with more money.
+
+Regards,
+
+DonationCorp Ltd."""
